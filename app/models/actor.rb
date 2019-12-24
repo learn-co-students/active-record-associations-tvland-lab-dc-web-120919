62482de37e500157696627776d9bf1a1312b4767
John Louis Rumingan <jmrumingan@gmail.com>
2019-12-23 22:37:04 -0500

    Finished all, build_network not working

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+
+    has_many :characters
+    has_many :shows, through: :characters 
   
+    def full_name
+
+        return self.first_name + " " + self.last_name 
+    end 
+
+    def list_roles
+
+        list = Character.all.select {|character| character.actor == self }
+        list.map {|character| character.name + " - " + character.show.name}
+    end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
   
+    has_many :characters
+    has_many :actors, through: :characters 
+    belongs_to :network
+
+    def actors_list 
+
+        #enumerable uses actor.full_name from actor method 
+        return self.actors.map {|actor| actor.full_name}
+    end 
 end
